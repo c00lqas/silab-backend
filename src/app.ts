d05d33d10471c416f9224e75e64527ddde5151f6
firoos18 <itsfiroos@gmail.com>
2025-05-03 22:46:21 +0700

    feat [collaborator] : adding collaborator functionality

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import ActivationRoute from "../src/routes/activation.route";
 import MeetingRoute from "../src/routes/meeting.route";
 import UserRoute from "../src/routes/user.route";
 import AnnouncementRoute from "../src/routes/announcement.route";
+import CollaboratorRoute from "../src/routes/collaborator.route";
 import cors from "cors";
 
 const app = express();
@@ -23,5 +24,6 @@ app.use("/activation", ActivationRoute);
 app.use("/meeting", MeetingRoute);
 app.use("/user", UserRoute);
 app.use("/announcement", AnnouncementRoute);
+app.use("/collaborator", CollaboratorRoute);
 
 app.use(errorHandler);
